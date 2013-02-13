72367e37de5056c0acf86018abcdd45a1bc97120
Ted Wahrburg <ted.wahrburg@gmail.com>
2013-02-13 09:50:15 -0500

    Added Hand Detect Interrupt

--- Arduino/CandyDispenser/CandyDispenser/CandyDispenser.ino
+++ Arduino/CandyDispenser/CandyDispenser/CandyDispenser.ino
@@ -1,13 +1,18 @@
-int dispenseTime = 500;
-int motor = 6;
+int dispenseTime = 700;
+
+
+int motor = 14;
 int handSensor = 5;
 int tableDetect = 3;
-int buffer = 0;
-int candy = 0;
-int i = 0;
 int rLED = 9;
 int gLED = 10;
 int bLED = 11;
+
+int buffer = 0;
+int candy = 0;
+int i = 0;
+int handDetected = 0;
+
 
 void setup() {
   // put your setup code here, to run once:
@@ -21,10 +26,20 @@ pinMode(gLED,OUTPUT);
 pinMode(bLED,OUTPUT);
 
 Serial.begin(9600);
+
+attachInterrupt(handSensor, handDetect_func, RISING);
 }
 
 void loop() {
   // put your main code here, to run repeatedly: 
+  
+if (handDetected == 1)  
+{
+ Serial.println("YOLO");
+ handDetected ==0;
+} 
+    
+  
   
   if (Serial.available() > 0)
   {
@@ -33,37 +48,28 @@ void loop() {
 
    if (buffer == 'G') 
      i = 1;
-   else if (buffer == 'U') 
-   {
-     if (i == 1)
-      i =2;
-     else
-      i = 0;      
-   }
-   else if (buffer == 'M') 
-   {
-     if (i == 2)
-      i =3;
-     else
-      i = 0;      
-   }
- 
-   else if (buffer == '\n') 
-   {
-     if (i==4)  
-	//attachInterrupt(handSensor, handDetect_func, RISING);     
-      if ( dispenseCandy(candy) )
-       Serial.println('Fin');
-       
-     i = 0; 
      
-    }
-    
-    else if (i == 3)
-    {
+   else if (buffer == 'U') {
+     if (i == 1)  i =2;
+     else  i = 0; }
+     
+   else if (buffer == 'M') {
+     if (i == 2) i =3;
+      else i = 0;  }
+      
+   else if (i == 3) {
      candy = buffer;
      i = 4;
     }
+ 
+   else if (buffer == '\n') {
+     if (i==4)  
+      if ( dispenseCandy(candy) )
+       Serial.println('Fin');
+     i = 0; 
+    }
+    
+
     
   }
   
@@ -80,6 +86,12 @@ int dispenseCandy(int howMuchCandy)
  }
  else
   return 0;
+}
+
+void handDetect_func()
+{
+handDetected = 1;  
+  
 }
 
 
