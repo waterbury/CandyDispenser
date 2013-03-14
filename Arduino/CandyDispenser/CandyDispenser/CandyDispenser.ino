0e38ae44626a966b446b5bb02b2e30c2f53a7610
Ted Wahrburg <ted.wahrburg@gmail.com>
2013-03-13 21:36:25 -0400

    Ben "Oops"

--- Arduino/CandyDispenser/CandyDispenser/CandyDispenser.ino
+++ Arduino/CandyDispenser/CandyDispenser/CandyDispenser.ino
@@ -1,3 +1,6 @@
+#include <Wire.h>
+#include <Adafruit_NFCShield_I2C.h>
+
 int brightness = 0;    // how bright the LED is
 int fadeAmount = 5;    // how many points to fade the LED by
 
@@ -8,6 +11,7 @@ int candyDispensed = 0;
 
 int motor = 14;
 int handSensor = 5;
+int NFC_IRQ = 3;
 int tableDetect = 8;
 int rLED = 9;
 int gLED = 10;
@@ -18,9 +22,26 @@ int candy = 0;
 int i = 0;
 int handDetected = 0;
 
+  //Authorized UIDs
+  uint8_t auth_uids[][7] = {
+    {0xFE, 0xE2, 0x70, 0x5A, 0x00, 0x00, 0x00}, // David Hand
+    {0xCE, 0xE5, 0x70, 0x5A, 0x00, 0x00, 0x00}, // Lance Hand
+    {0x43, 0x7C, 0x15, 0x34, 0x00, 0x00, 0x00}, // Lance Phone
+    {0x2E, 0x1D, 0x73, 0xBE, 0x00, 0x00, 0x00}, // iShortBus Phone Sticker
+    {0x04, 0xC4, 0x5C, 0x1A, 0xD9, 0x26, 0x80}, // bentruck Phone Sticker
+    {0xEE, 0xCF, 0x17, 0xDB, 0x00, 0x00, 0x00}, // Door Open Chip
+    {0xCE, 0x2D, 0x74, 0xBE, 0x00, 0x00, 0x00}, //Conner Brooks Sticker
+    {0x1E, 0x83, 0x74, 0xBE, 0x00, 0x00, 0x00} //Tony Door Sticker
+};
+Adafruit_NFCShield_I2C nfc(NFC_IRQ, 7);
 
 void setup() {
   // put your setup code here, to run once:
+  
+
+  
+pinMode(NFC_IRQ,INPUT);
+
 pinMode(motor,OUTPUT);
 pinMode(handSensor,INPUT);
 pinMode(tableDetect,INPUT);
@@ -35,12 +56,38 @@ pinMode(bLED,OUTPUT);
 Serial.begin(9600);
 
 attachInterrupt(0, handDetect_func, RISING);
+
+
+
+  nfc.begin();
+
+  uint32_t versiondata = nfc.getFirmwareVersion();
+  if (! versiondata) {
+    Serial.print("Didn't find PN53x board");
+    while (1); // halt
+  }
+  
+  // Got ok data, print it out!
+  Serial.print("Found chip PN5"); Serial.println((versiondata>>24) & 0xFF, HEX); 
+  Serial.print("Firmware ver. "); Serial.print((versiondata>>16) & 0xFF, DEC); 
+  Serial.print('.'); Serial.println((versiondata>>8) & 0xFF, DEC);
+  
+  // Set the max number of retry attempts to read from a card
+  // This prevents us from waiting forever for a card, which is
+  // the default behaviour of the PN532.
+  nfc.setPassiveActivationRetries(0x01);
+  
+  // configure board to read RFID tags
+  nfc.SAMConfig();
+    
+  Serial.println("Waiting for an ISO14443A card");
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly: 
 
-while (digitalRead(tableDetect) ==   HIGH)
+/*while (digitalRead(tableDetect) ==   HIGH)
 {
 digitalWrite(bLED, 0);    
 digitalWrite(gLED, 0);    
@@ -50,7 +97,54 @@ delay(250);
 analogWrite(rLED, 0);
 delay(250);  
   
-}  
+} */ 
+
+  uint8_t success;
+  uint8_t uid[] = { 0, 0, 0, 0, 0, 0, 0 };
+  uint8_t uidLength;
+
+  if (nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength))
+  {
+
+    Serial.print("Entry Request for UID: ");
+    nfc.PrintHex(uid, uidLength);
+
+    uint32_t i;
+    uint32_t b;
+    int num_uids = sizeof(auth_uids)/sizeof(auth_uids[0]);
+    Serial.println(num_uids);
+    for (i=0; i < num_uids; i++){
+        Serial.print("Comparing UID to: ");
+        nfc.PrintHex(auth_uids[i], 7);
+        for (b=0; b < 7; b++)
+        {
+            if (uid[b] == auth_uids[i][b])
+            {
+                if (b == 6)
+                {
+                   // digitalWrite(13, HIGH);
+                   dispenseCandy(5);
+                   
+                    Serial.println("Unlocked!");
+                    delay(10000);
+                   // digitalWrite(13, LOW);
+                    Serial.println("Locked!");
+                    i=num_uids;
+                }
+            } 
+            else 
+            {
+                Serial.println("Failed!");
+                //b=7;
+            }
+        }
+    }
+
+  }
+
+
+
+
  
   
   
