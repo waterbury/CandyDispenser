19e8906da8563cc37d6952bf696cdfa646b3a614
Ted <ted@sonybury.(none)>
2013-09-29 22:29:24 -0400

    Removed NFC Code, and added spill mode

--- Arduino/CandyDispenser/CandyDispenser/CandyDispenser.ino
+++ Arduino/CandyDispenser/CandyDispenser/CandyDispenser.ino
@@ -1,6 +1,3 @@
-#include <Wire.h>
-#include <Adafruit_NFCShield_I2C.h>
-
 int brightness = 0;    // how bright the LED is
 int fadeAmount = 5;    // how many points to fade the LED by
 
@@ -11,7 +8,6 @@ int candyDispensed = 0;
 
 int motor = 14;
 int handSensor = 5;
-int NFC_IRQ = 3;
 int tableDetect = 8;
 int rLED = 9;
 int gLED = 10;
@@ -21,28 +17,10 @@ int buffer = 0;
 int candy = 0;
 int i = 0;
 int handDetected = 0;
-int cardDetected = 0;
 
-  //Authorized UIDs
-  uint8_t auth_uids[][7] = {
-    {0xFE, 0xE2, 0x70, 0x5A, 0x00, 0x00, 0x00}, // David Hand
-    {0xCE, 0xE5, 0x70, 0x5A, 0x00, 0x00, 0x00}, // Lance Hand
-    {0x43, 0x7C, 0x15, 0x34, 0x00, 0x00, 0x00}, // Lance Phone
-    {0x2E, 0x1D, 0x73, 0xBE, 0x00, 0x00, 0x00}, // iShortBus Phone Sticker
-    {0x04, 0xC4, 0x5C, 0x1A, 0xD9, 0x26, 0x80}, // bentruck Phone Sticker
-    {0xEE, 0xCF, 0x17, 0xDB, 0x00, 0x00, 0x00}, // Door Open Chip
-    {0xCE, 0x2D, 0x74, 0xBE, 0x00, 0x00, 0x00}, //Conner Brooks Sticker
-    {0x1E, 0x83, 0x74, 0xBE, 0x00, 0x00, 0x00} //Tony Door Sticker
-};
-Adafruit_NFCShield_I2C nfc(NFC_IRQ, 7);
 
 void setup() {
   // put your setup code here, to run once:
-  
-
-  
-pinMode(NFC_IRQ,INPUT);
-
 pinMode(motor,OUTPUT);
 pinMode(handSensor,INPUT);
 pinMode(tableDetect,INPUT);
@@ -57,39 +35,12 @@ pinMode(bLED,OUTPUT);
 Serial.begin(9600);
 
 attachInterrupt(0, handDetect_func, RISING);
-
-
-
-  nfc.begin();
-/*
-  uint32_t versiondata = nfc.getFirmwareVersion();
-  if (! versiondata) {
-    Serial.print("Didn't find PN53x board");
-    while (1); // halt
-  }
-  
-  // Got ok data, print it out!
-  Serial.print("Found chip PN5"); Serial.println((versiondata>>24) & 0xFF, HEX); 
-  Serial.print("Firmware ver. "); Serial.print((versiondata>>16) & 0xFF, DEC); 
-  Serial.print('.'); Serial.println((versiondata>>8) & 0xFF, DEC);
-  
-  // Set the max number of retry attempts to read from a card
-  // This prevents us from waiting forever for a card, which is
-  // the default behaviour of the PN532.
-  nfc.setPassiveActivationRetries(0xFF);
-  
-  // configure board to read RFID tags
-  */
-  nfc.SAMConfig();
-    
-  Serial.println("Waiting for an ISO14443A card");
-
 }
 
 void loop() {
   // put your main code here, to run repeatedly: 
 
-/*while (digitalRead(tableDetect) ==   HIGH)
+while (digitalRead(tableDetect) ==   HIGH)
 {
 digitalWrite(bLED, 0);    
 digitalWrite(gLED, 0);    
@@ -99,122 +50,8 @@ delay(250);
 analogWrite(rLED, 0);
 delay(250);  
   
-} */ 
-
-
-
-  //Authorized UIDs
-  uint8_t auth_uids[][7] = {
-    {0xFE, 0xE2, 0x70, 0x5A, 0x00, 0x00, 0x00}, // David Hand
-    {0xCE, 0xE5, 0x70, 0x5A, 0x00, 0x00, 0x00}, // Lance Hand
-    {0x43, 0x7C, 0x15, 0x34, 0x00, 0x00, 0x00}, // Lance Phone
-    {0x2E, 0x1D, 0x73, 0xBE, 0x00, 0x00, 0x00}, // iShortBus Phone Sticker
-    {0x04, 0xC4, 0x5C, 0x1A, 0xD9, 0x26, 0x80}, // bentruck Phone Sticker
-    {0xEE, 0xCF, 0x17, 0xDB, 0x00, 0x00, 0x00}, // Door Open Chip
-    {0xCE, 0x2D, 0x74, 0xBE, 0x00, 0x00, 0x00}, //Conner Brooks Sticker
-    {0x1E, 0x83, 0x74, 0xBE, 0x00, 0x00, 0x00} //Tony Door Sticker
-};
-
-  uint8_t success;
-  uint8_t uid[] = { 0, 0, 0, 0, 0, 0, 0 };
-  uint8_t uidLength;
-
-  if (nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength))
-  {
-     handDetected = 0;
-
-    Serial.print("Entry Request for UID: ");
-    nfc.PrintHex(uid, uidLength);
-
-    uint32_t i;
-    uint32_t b;
-    int num_uids = sizeof(auth_uids)/sizeof(auth_uids[0]);
-    Serial.println(num_uids);
-    for (i=0; i < num_uids; i++)
-    {
-        Serial.print("Comparing UID to: ");
-        nfc.PrintHex(auth_uids[i], 7);
-        for (b=0; b < 5; b++)
-        {
-            if (uid[b] == auth_uids[i][b])
-            {
-                if (b == 4)
-                {
-                 Serial.println("Match"); 
-                 cardDetected = 1;
-                 i=num_uids;
-              }
-             else 
-             {
-                Serial.println("No Match!");
-                cardDetected = 0;             
-                b=7;
-            }
-        }
-    }
-
-  }
-  
-  if (cardDetected == 1)
-  {                     
-                 digitalWrite(bLED, 0);        
-                 digitalWrite(rLED, 0);    
-     
-                 previousTime = millis();
-                 candyDispensed = 0;
-                 Serial.println("Waiting for hand");     
-     
-                 while( ((millis() - previousTime) < 10000) && candyDispensed == 0)
-                 {
-                  analogWrite(gLED, brightness);   
-                  brightness = brightness + fadeAmount;
-                  if (brightness == 0 || brightness == 255) {
-                   fadeAmount = -fadeAmount ; }
-                  delay(30);  
-     
-                  if (handDetected == 1)  
-                  {
-                   if ( dispenseCandy(candy) )
-                   Serial.println("Fin");
-                   handDetected = 0; 
-                   candyDispensed = 1;
-                  }
-        
-                 } 
-    
-  }
-  else
-  {
-    digitalWrite(rLED, HIGH);    
-    delay(150);
-    digitalWrite(rLED, LOW);
-    delay(150);
-    digitalWrite(rLED, HIGH);
-    delay(150);
-    digitalWrite(rLED, LOW);
-    delay(150);
-    digitalWrite(rLED, HIGH);
-    delay(150);
-    digitalWrite(rLED, LOW);
-    delay(150);
-    digitalWrite(rLED, HIGH);
-    delay(150);
-    digitalWrite(rLED, LOW);
-    delay(150); 
-  }
-  
-  
-  }
-
-
-
-
-
-
-
-
-
-/* 
+}  
+ 
   
   
 if (handDetected == 1)  
@@ -315,6 +152,13 @@ delay(30);
      }
         
      }
+     else if (i==4 && candy >5 && candy <=9)
+       {
+         dispenseCandy(candy-5);
+         
+       }
+   
+     
   else
    Serial.println("Out of bounds");
         
@@ -323,7 +167,7 @@ delay(30);
     
 
     
-  }*/
+  }
   
 }
 
@@ -360,5 +204,6 @@ void handDetect_func()
 handDetected = 1;  
   
 }
+
 
 
